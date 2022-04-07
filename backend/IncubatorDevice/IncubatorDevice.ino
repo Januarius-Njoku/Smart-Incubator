1f37f661fc5a18de591fba35b93c37e7f5de1ba1
Nsisong <nsisong@Nsisongs-MacBook-Air.local>
2022-04-07 16:05:19 +0100

    added oled and four button swith for menue control

--- backend/IncubatorDevice/IncubatorDevice.ino
+++ backend/IncubatorDevice/IncubatorDevice.ino
@@ -1,3 +1,13 @@
+/* Please see documentation and circuit design
+ *  
+ * Tx on esp == Value +
+ * Rx on esp == value -
+ * D6 on esp == menue +
+ * D5 on esp == value +
+ * 
+ * 
+ */
+
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h> 
 #include <WiFiClientSecure.h>
@@ -46,13 +56,13 @@ float inputVoltage    = 0;
 float maxPower        = 0;
 float heaterOntime    = 0;
 
-//int sw[] = {d4, d5, d6, s3};
+
 //int relay d3;
 //int dht d0;
 //int sound A0;
 //int servo1 d7;
 //int servo d8;
-//int oled sda d2, sck d1
+
 
 
 bool accesspoint = true;
@@ -61,6 +71,22 @@ int milliseconds=0;
 
 //Configuration Variable
 const char *fingerprint PROGMEM = "16 5B 4B A3 A7 8A 59 C6 14 44 CA FF 3E AF F4 7C 70 7D B1 9F";
+
+//Input & Button Logic
+const int numOfInputs = 4;
+const int inputPins[numOfInputs] = {1, 14, 12, 3};
+int inputState[numOfInputs];
+int lastInputState[numOfInputs] = {LOW,LOW,LOW,LOW};
+bool inputFlags[numOfInputs] = {LOW,LOW,LOW,LOW};
+long lastDebounceTime[numOfInputs] = {0,0,0,0};
+long debounceDelay = 5;
+
+//OLED Menu Logic
+const int numOfScreens = 5;// number of menues
+int currentScreen = 0;
+String screens[numOfScreens][1] = {{"Tempreature"}, {"humidity"}, 
+  {"inputVoltage"},{"maxPower"},{"heaterOntime"}};
+int parameters[numOfScreens];
 
 int ledPin =2;
 int dataPin =0;
@@ -154,11 +180,14 @@ void reconnect() {
 void setup() {
   Serial.begin(115200);
 
-  //setup oled 
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;);
+    for(int i = 0; i < numOfInputs; i++) {
+    pinMode(inputPins[i], INPUT);
+    digitalWrite(inputPins[i], HIGH); // pull-up 20k
   }
+
+  //setup oled 
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+
   display.clearDisplay();
 
   display.setTextSize(1);
@@ -167,7 +196,7 @@ void setup() {
   // Display static text
   display.println("smartIncubator");
   display.display();
-  delay(500);
+  delay(5000);
   display.clearDisplay();
 
   //setup broker
@@ -209,4 +238,76 @@ void loop() {
         ispublished =   client.publish("data/monitor/incubatorStatus", msg);
   }
 
+  setInputFlags();
+  resolveInputFlags();
+
+}
+
+void setInputFlags() {
+  for(int i = 0; i < numOfInputs; i++) {
+    int reading = digitalRead(inputPins[i]);
+    if (reading != lastInputState[i]) {
+      lastDebounceTime[i] = millis();
+    }
+    if ((millis() - lastDebounceTime[i]) > debounceDelay) {
+      if (reading != inputState[i]) {
+        inputState[i] = reading;
+        if (inputState[i] == HIGH) {
+          inputFlags[i] = HIGH;
+        }
+      }
+    }
+    lastInputState[i] = reading;
+  }
+}
+
+void resolveInputFlags() {
+  for(int i = 0; i < numOfInputs; i++) {
+    if(inputFlags[i] == HIGH) {
+      inputAction(i);
+      inputFlags[i] = LOW;
+      printScreen();
+    }
+  }
+}
+
+void inputAction(int input) {
+  if(input == 0) {
+    if (currentScreen == 0) {
+      currentScreen = numOfScreens-1;
+    }else{
+      currentScreen--;
+    }
+  }else if(input == 1) {
+    if (currentScreen == numOfScreens-1) {
+      currentScreen = 0;
+    }else{
+      currentScreen++;
+    }
+  }else if(input == 2) {
+    parameterChange(0);
+  }else if(input == 3) {
+    parameterChange(1);
+  }
+}
+
+void parameterChange(int key) {
+  if(key == 0) {
+    parameters[currentScreen]++;
+  }else if(key == 1) {
+    parameters[currentScreen]--;
+  }
+}
+
+void printScreen() {
+  
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 2);
+  display.println(screens[currentScreen][0]);
+  display.setCursor(0,10);
+  display.println(parameters[currentScreen]);
+  display.display();
+  display.clearDisplay();
 }
